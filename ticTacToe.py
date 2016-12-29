2207e7ed7a5552a334675e398681d53f384abbf0
Jacob <jacobgoldberg22@gmail.com>
2016-12-29 14:46:41 -0500

    Redoing the entire program, deleted content within main, and reorganized

--- ticTacToe.py
+++ ticTacToe.py
@@ -2,7 +2,7 @@ theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
 			'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
 			'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
-def printBoard(board):
+def printBoard(board): # draws the tic tac toe board on the screen
 	print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
 	print('-+-+-')
 	print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
@@ -10,14 +10,13 @@ def printBoard(board):
 	print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
 
-def turnSwitcher():
+def turnSwitcher(): # decides whose turn it is
 	if turn == 'X':
 		turn = 'O'
 	else:
 		turn = 'X'
 
-
-"""def moveSelector():
+def moveSelector(): # fills in the space selected by the user whose turn it is
 	printBoard(theBoard)
 	print('Turn for ' +turn+ '. Move on which space?')
 	move = input()
@@ -25,41 +24,38 @@ def turnSwitcher():
 		print("This space is filled, please pick a different space!")
 	theBoard[move] = turn    
 	printBoard(theBoard)
-	turnSwitcher()"""
 
-def winningCombos():
-	winner = False
-	while winner not True:
-		#X possibilities vertical
+
+def winningCombos(): # deteremines if either user X or user Y has achieved a winning combination
+	threeInARow = False
+	while not threeInARow == True:
+		# X possibilities vertical
 		if (((theBoard(top-L) == X) and (theBoard(mid-L) == X) and (theBoard(low-L) == X)) or ((theBoard(top-M) == X) and (theBoard(mid-M) == X) and (theBoard(low-M) == X)) or ((theBoard(top-M) == X) and (theBoard(mid-R) == X) and (theBoard(low-R) == X))):
-			winner = True
-		#Y possibilities vertical
+			threeInARow = 'X'
+		# Y possibilities vertical
 		if (((theBoard(top-L) == Y) and (theBoard(mid-L) == Y) and (theBoard(low-L) == Y)) or ((theBoard(top-M) == Y) and (theBoard(mid-M) == Y) and (theBoard(low-M) == Y)) or ((theBoard(top-M) == Y) and (theBoard(mid-R) == Y) and (theBoard(low-R) == Y))):
-			winner = True
-		#X possibilities horizontal
+			threeInARow = 'Y'
+		# X possibilities horizontal
 		if (theBoard(top-L) == X and theBoard(top-M)==X and theBoard(top-R) == X) or (theBoard(mid-L) == X and theBoard(mid-M)==X and theBoard(mid-R) == X) or (theBoard(low-L) == X and theBoard(low-M)==X and theBoard(low-R) == X):
-			winner = True
-		#Y possibilities  horizontal
+			threeInARow = 'X'
+		# Y possibilities  horizontal
 		if (theBoard(top-L) == Y and theBoard(top-M)==Y and theBoard(top-R) == Y) or (theBoard(mid-L) == Y and theBoard(mid-M)==Y and theBoard(mid-R) == Y) or (theBoard(low-L) == Y and theBoard(low-M)==Y and theBoard(low-R) == Y):
-			winner = True
-		#X possibilities 
+			threeInARow = 'Y'
+		# X possibilities diagnol
+		if (theBoard(top-L) == X and theBoard(mid-M) == X and theBoard(low-R) == X) or (theBoard(top-R) == X and theBoard(mid-M) == X and theBoard(low-L) == X):
+			threeInARow = 'X'
+		# Y possibilities diagnol
+		if (theBoard(top-L) == Y and theBoard(mid-M) == Y and theBoard(low-R) == Y) or (theBoard(top-R) == Y and theBoard(mid-M) == Y and theBoard(low-L) == Y):
+			threeInARow = 'Y'
+
 
 		
 
 def main():
-	printBoard(theBoard)
-	turn = 'X'
-	for i in range(9):
-		move = input('Turn for' +turn+ '. Move on which space?')
-		if theBoard.get(move) == 'X' or theBoard.get(move) == 'O':
-			print("This space is filled, please pick a different space!")
-		else:
-			theBoard[move] = turn #fills in the "move" tic tac toe space with the appropriate turn character
-			turnSwitcher()
-		printBoard(theBoard)
-		
+	printBoard()
 
-	
+
+
 
 main()
 
