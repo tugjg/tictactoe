66fc65c91f473266e31b320143d629cf73dec86c
Jacob <jacobgoldberg22@gmail.com>
2016-12-30 13:32:45 -0500

    Best working script so far. Still needs to get the move checker part working.

--- ticTacToe.py
+++ ticTacToe.py
@@ -10,39 +10,42 @@ def printBoard(board): # draws the tic tac toe board on the screen
 	print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
 def moveMaker(turn): # fills in the space selected by the user whose turn it is
-	move = input("Enter a space on the grid for your " +turn+ " mark to be placed.")
+	move = input("Enter a space on the grid for your " +turn+ " mark to be placed. \n")
+	print("")
 	theBoard[move] = turn
 	return move
 
 def moveChecker(move): # checks to see if the space is already filled by another piece
-	if theBoard.get(move) == 'X':
+	if theBoard.get(move) == 'X' or theBoard.get(move) == 'O':
 		print("This space is filled with an X. Please pick a different space!")
 		return False
-	elif theBoard.get(move) == 'O':
-		print("This space is already filled with an O. Please try a different space!")
-		return False
 	else:
-		print("Move is a success!")
 		return True
 
 def winningCombos(): # deteremines if either user X or user Y has achieved a winning combination
 	# X possibilities vertical
 	if (((theBoard['top-L'] == 'X') and (theBoard['mid-L'] == 'X') and (theBoard['low-L'] == 'X')) or ((theBoard['top-M'] == 'X') and (theBoard['mid-M'] == 'X') and (theBoard['low-M'] == 'X')) or ((theBoard['top-M'] == 'X') and (theBoard['mid-R'] == 'X') and (theBoard['low-R'] == 'X'))):
+		print('The winner is X!')
 		return True
 	# Y possibilities vertical'
 	if (((theBoard['top-L'] == 'Y') and (theBoard['mid-L'] == 'Y') and (theBoard['low-L'] == 'Y')) or ((theBoard['top-M'] == 'Y') and (theBoard['mid-M'] == 'Y') and (theBoard['low-M'] == 'Y')) or ((theBoard['top-M'] == 'Y') and (theBoard['mid-R'] == 'Y') and (theBoard['low-R'] == 'Y'))):
+		print('The winner is O!')
 		return True
 	# X possibilities horizontal
 	if (((theBoard['top-L'] == 'X') and (theBoard['top-M']== 'X') and (theBoard['top-R'] == 'X')) or ((theBoard['mid-L'] == 'X') and (theBoard['mid-M']== 'X') and (theBoard['mid-R'] == 'X')) or ((theBoard['low-L'] == 'X') and (theBoard['low-M'] == 'X') and (theBoard['low-R'] == 'X'))):
+		print('The winner is X!')
 		return True
 	# Y possibilities  horizontal
 	if (((theBoard['top-L'] == 'Y') and (theBoard['top-M']== 'Y') and (theBoard['top-R'] == 'Y')) or ((theBoard['mid-L'] == 'Y') and (theBoard['mid-M']== 'Y') and (theBoard['mid-R'] == 'Y')) or ((theBoard['low-L'] == 'Y') and (theBoard['low-M'] =='Y') and (theBoard['low-R'] == 'Y'))):
+		print('The winner is O!')
 		return True
 	# X possibilities diagonal
 	if (((theBoard['top-L'] == 'X') and (theBoard['mid-M'] == 'X') and (theBoard['low-R'] == 'X')) or ((theBoard['top-R'] == 'X') and (theBoard['mid-M'] == 'X') and (theBoard['low-L'] == 'X'))):
+		print('The winner is X!')
 		return True
 	# Y possibilities diagonal
 	if (((theBoard['top-L'] == 'Y') and (theBoard['mid-M'] == 'Y') and (theBoard['low-R'] == 'Y')) or ((theBoard['top-R'] == 'Y') and (theBoard['mid-M'] == 'Y') and (theBoard['low-L'] == 'Y'))):
+		print('The winner is O!')
 		return True
 	else:
 		return False
@@ -55,22 +58,21 @@ def turnSwitcher(turn):
 	return turn
 
 
+
 def main():
-	print("Welcome to Tic Tac Toe")
+	print("Welcome to Tic Tac Toe!")
 	printBoard(theBoard)
-	turn = 'X'
-	winner = ''
+	turn = 'X' 
 	move = ''
-	while winningCombos() is False:
-		print('It is ' +turn+ ' \'s turn. Enter a place to move. Options: top-L, mid-M, low-R, etc..') # instructions
-		while moveChecker(move) is True:
-			winningCombos()
+	while not winningCombos() is True:
+		if moveChecker(move) is True:
+			winner = winningCombos()
 			moveMaker(turn) # calls the function to place the appropriate marker
 			moveChecker(move)
 			printBoard(theBoard) # prints the newly marked board
 			turn = turnSwitcher(turn)
 		else:
-			winningCombos()
+			winner = winningCombos()
 			moveMaker(turn)
 			moveChecker(move)
 			printBoard(theBoard)
